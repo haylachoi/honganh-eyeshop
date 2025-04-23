51d81527fed051880a08154ab550003d0d768e25
haylachoi <haylachoi@gmail.com>
2025-04-23 21:46:02 +0700

    fix some space in profile layout

--- app/(home)/(profile)/layout.tsx
+++ app/(home)/(profile)/layout.tsx
@@ -7,12 +7,14 @@ export default function ProfileLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex flex-col lg:flex-row gap-4 container">
-      <div className="lg:w-[400px] xl:w-[500px] border border-foreground p-2 space-y-4">
-        <div className="border-b border-input pb-2">
-          <CustomerAvatarProvider />
+    <div className="flex flex-col justify-center lg:flex-row gap-4 container">
+      <div className="lg:w-[400px] xl:w-[500px] border border-foreground px-2 py-4 space-y-4">
+        <div className="mx-auto max-w-xl space-y-4">
+          <div className="border-b border-input pb-2">
+            <CustomerAvatarProvider />
+          </div>
+          <ProfileSidebar className="flex gap-y-4 md:flex-col" />
         </div>
-        <ProfileSidebar className="flex gap-y-4 md:flex-col" />
       </div>
       <div className="px-2 border border-foreground grow">{children}</div>
     </div>
