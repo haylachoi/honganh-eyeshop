72bb0d173ce24ec5a269719babe1d20eb71f6579
haylachoi <haylachoi@gmail.com>
2025-05-07 23:28:03 +0700

    feat: add zoom image in product page

--- app/(home)/c/[categorySlug]/[productSlug]/_components/image-section.tsx
+++ app/(home)/c/[categorySlug]/[productSlug]/_components/image-section.tsx
@@ -8,15 +8,49 @@ import { cn } from "@/lib/utils";
 const ImageSection = ({ product }: { product: ProductType }) => {
   const images = product.variants.flatMap((variant) => variant.images);
   const { currentImage, setCurrentImage } = React.use(TopContext);
+
+  const [zoomPosition, setZoomPosition] = React.useState({ x: 50, y: 50 });
+  const [isZoomVisible, setIsZoomVisible] = React.useState(false);
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const { left, top, width, height } =
+      e.currentTarget.getBoundingClientRect();
+    const x = ((e.clientX - left) / width) * 100;
+    const y = ((e.clientY - top) / height) * 100;
+    setZoomPosition({ x, y });
+  };
+
   return (
     <div className="flex flex-col gap-2">
-      <Image
-        className="w-full aspect-[18/9] object-cover"
-        src={currentImage}
-        alt={product.name}
-        width={500}
-        height={250}
-      />
+      <div
+        className="relative w-full aspect-[18/9] overflow-hidden"
+        onMouseMove={handleMouseMove}
+        onMouseEnter={() => setIsZoomVisible(true)}
+        onMouseLeave={() => setIsZoomVisible(false)}
+      >
+        <Image
+          className="w-full h-full object-cover"
+          src={currentImage}
+          alt={product.name}
+          width={600}
+          height={300}
+        />
+
+        {/* Zoom overlay */}
+        <div
+          className={cn(
+            "absolute inset-0 pointer-events-none z-10 transition-opacity duration-300 ease-out",
+            isZoomVisible ? "opacity-100" : "opacity-0",
+          )}
+          style={{
+            backgroundImage: `url(${currentImage})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "200%", // Zoom scale
+            backgroundPosition: `${zoomPosition.x}% ${zoomPosition.y}%`,
+          }}
+        />
+      </div>
+
       <CarouselImages
         isDotButtonVisible={false}
         isArrowButtonVisible={false}
@@ -26,7 +60,7 @@ const ImageSection = ({ product }: { product: ProductType }) => {
             src={image.imageUrl}
             alt={product.name}
             className={cn(
-              "w-full aspect-[18/9] object-cover cursor-pointer",
+              "w-full aspect-[18/9] object-cover cursor-pointer transition-all hover:scale-105",
               image.imageUrl === currentImage &&
                 "p-1 border-2 border-foreground",
             )}
