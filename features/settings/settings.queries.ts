d1288a14994457b5782a545d25de0b2178db2282
haylachoi <haylachoi@gmail.com>
2025-05-15 00:56:21 +0700

    fix: not use cookie when get settings

--- features/settings/settings.queries.ts
+++ features/settings/settings.queries.ts
@@ -1,12 +1,10 @@
 import next_cache from "@/cache";
 import { NotFoundError } from "@/lib/error";
-import { getAuthQueryClient } from "@/lib/query";
+import { safeQuery } from "@/lib/query";
 import { Resource } from "../authorization/authorization.constants";
 
 const resource: Resource = "settings";
-export const getSettings = getAuthQueryClient({
-  resource,
-}).query(async () => {
+export const getSettings = safeQuery.query(async () => {
   const result = await next_cache.settings.getSettings();
   if (!result) {
     throw new NotFoundError({
