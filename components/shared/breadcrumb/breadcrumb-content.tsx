7c2fd0d4693345bba7056d2474c41bbd8a738d42
haylachoi <haylachoi@gmail.com>
2025-05-21 22:44:26 +0700

    refactor: refactor breadcrumb

--- components/shared/breadcrumb/breadcrumb-content.tsx
+++ components/shared/breadcrumb/breadcrumb-content.tsx
@@ -4,36 +4,41 @@ import { House } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-export const BreadcrumbContent = ({ map }: { map: Record<string, string> }) => {
+function generateBreadcrumbs(
+  pathname: string,
+  breadcrumbsPath: Record<string, { label: string; href: string }>,
+) {
+  const breadcrumbs: { label: React.ReactNode; href: string }[] = [
+    { label: <House />, href: "/" },
+  ];
+
+  const segments = pathname.split("/").filter(Boolean); // ignore empty
+  segments.forEach((segment) => {
+    const matched = Object.entries(breadcrumbsPath).find(
+      ([key]) => key === segment,
+    );
+    if (matched) {
+      breadcrumbs.push(matched[1]);
+    }
+  });
+
+  return breadcrumbs;
+}
+
+export const BreadcrumbContent = ({
+  breadcrumbsMap,
+}: {
+  breadcrumbsMap: Record<string, { label: string; href: string }>;
+}) => {
   const pathname = usePathname();
   if (!pathname?.[1]) return null;
-  if (pathname.startsWith("/checkout")) return null;
 
-  // todo: should auto replace blog
-  const segments = pathname
-    .split("/")
-    .filter(Boolean) // ignore empty
-    .map((s) => (s === "b" ? "blogs" : s));
-
-  const breadcrumbs = segments
-    .slice(0, -1)
-    .map((segment, index) => {
-      const href = "/" + segments.slice(0, index + 1).join("/");
-      return {
-        label: map[segment] || segment, // Lấy tên từ map, fallback là segment gốc
-        href,
-      };
-    })
-    .filter((segment) => segment.href !== "/c");
+  const breadcrumbs = generateBreadcrumbs(pathname, breadcrumbsMap);
+  console.log(breadcrumbsMap, breadcrumbs);
 
   return (
     <nav className="">
       <ul className="flex items-center gap-3">
-        <li className="after:content-['>'] flex items-center gap-1">
-          <Link aria-label="Home" href="/" className="hover:underline">
-            <House className="size-5" />
-          </Link>
-        </li>
         {breadcrumbs.map((crumb) => (
           <li
             key={crumb.href}
--- components/shared/breadcrumb/index.tsx
+++ components/shared/breadcrumb/index.tsx
@@ -1,17 +1,27 @@
 import { getAllCategories } from "@/features/categories/category.queries";
 import { BreadcrumbContent } from "./breadcrumb-content";
 
+const defaultBreadcrumbsMap: Record<string, { label: string; href: string }> = {
+  blogs: { label: "Tin tức", href: "/blogs" },
+  b: { label: "Tin tức", href: "/blogs" },
+};
 const Breadcrumb = async () => {
   const categoriesResult = await getAllCategories();
   const categories = categoriesResult.success ? categoriesResult.data : [];
-  const dic = {
-    blogs: "Tin tức",
-    b: "Tin tức",
-    ...Object.fromEntries(categories.map(({ slug, name }) => [slug, name])),
+
+  const breadcrumbsMap: Record<string, { label: string; href: string }> = {
+    ...defaultBreadcrumbsMap,
+    ...Object.fromEntries(
+      categories.map(({ slug, name }) => [
+        slug,
+        { label: name, href: `/c/${slug}` },
+      ]),
+    ),
   };
+
   return (
     <div className="container">
-      <BreadcrumbContent map={dic} />
+      <BreadcrumbContent breadcrumbsMap={breadcrumbsMap} />
     </div>
   );
 };
