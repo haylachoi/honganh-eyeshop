e49fce15520969cbec01879659601637fd85e6cc
haylachoi <haylachoi@gmail.com>
2025-05-22 16:32:14 +0700

    refactor: delete unuse route

--- app/(auth)/profile/page.tsx
+++
@@ -1,8 +0,0 @@
-import { auth } from "@/features/auth/auth.auth";
-
-const ProfilePage = async () => {
-  const user = await auth();
-  return <div>{JSON.stringify(user)}</div>;
-};
-
-export default ProfilePage;
