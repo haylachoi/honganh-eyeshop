d701065281f5f727118618ee4a8e73ecdb64ce28
haylachoi <haylachoi@gmail.com>
2025-05-28 16:31:35 +0700

    feat: add docker file

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
   experimental: {
     serverActions: {
       bodySizeLimit: "4mb",
