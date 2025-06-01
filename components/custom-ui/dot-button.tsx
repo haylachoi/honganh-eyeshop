a7b8585e442866994154acf13aa59f4d6f581aed
haylachoi <haylachoi@gmail.com>
2025-06-01 08:38:39 +0700

    style: change dot button in carousel

--- components/custom-ui/dot-button.tsx
+++ components/custom-ui/dot-button.tsx
@@ -14,18 +14,20 @@ const CarouselDotButton = ({
   onDotButtonClick,
 }: Props) => {
   return (
-    <div className={cn("flex items-center gap-4", className)}>
+    <div className={cn("flex items-center gap-1", className)}>
       {Array.from({ length: count }, (_, i) => i).map((n) => (
         <button
           key={n}
           onClick={() => onDotButtonClick(n)}
           aria-label={`slide ${n + 1}`}
           className={cn(
-            "size-[16px] bg-primary opacity-50 transition-all cursor-pointer",
+            "size-[24px] opacity-50 transition-all cursor-pointer flex items-center justify-center",
             current === n &&
               "scale-150 rotate-45 opacity-100 cursor-not-allowed",
           )}
-        ></button>
+        >
+          <div className="p-[5px] bg-primary"></div>
+        </button>
       ))}
     </div>
   );
